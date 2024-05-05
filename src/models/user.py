feadecfd212eeeb174247fdc047bdf972262ae38
okw98jn <j.ookawa.0417@gmail.com>
2024-05-05 14:37:19 +0900

    ユーザーモデル作成

---
+++ src/models/user.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlmodel import Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    email: str = Field(unique=True)
+    password: str
+    created_at: datetime = Field(default_factory=datetime.now, nullable=False)
+    updated_at: datetime = Field(
+        default_factory=datetime.now,
+        nullable=False,
+        sa_column_kwargs={"onupdate": datetime.now},
+    )
