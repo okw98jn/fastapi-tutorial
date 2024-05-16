e836504ad98a13a2540f8051849dba23827030e1
okw98jn <j.ookawa.0417@gmail.com>
2024-05-16 22:51:04 +0900

    db接続に処理追加

--- src/connector/db_connect.py
+++ src/connector/db_connect.py
@@ -1,10 +1,18 @@
 from sqlmodel import Session, create_engine
 
+from src.logger import logger
 from src.settings.db import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 
 def get_session():
-    with Session(engine) as session:
+    session = Session(engine)
+    try:
         yield session
+    except Exception as e:
+        logger.error(e)
+        session.rollback()
+        raise
+    finally:
+        session.close()
