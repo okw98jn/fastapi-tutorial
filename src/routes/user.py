abe6a24484168da995e45909465c5ce1b83a592b
okw98jn <j.ookawa.0417@gmail.com>
2024-05-16 22:50:31 +0900

    response_model設定

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,11 +1,22 @@
 from fastapi import APIRouter
 
 from src.controllers.user import UserController
+from src.models.user import UserPublic
 
 router = APIRouter(prefix="/users", tags=["users"])
 
-router.add_api_route("/", UserController.index, methods=["GET"])
-router.add_api_route("/{user_id}", UserController.show, methods=["GET"])
-router.add_api_route("/", UserController.create, methods=["POST"])
-router.add_api_route("/{user_id}", UserController.update, methods=["PUT"])
-router.add_api_route("/{user_id}", UserController.delete, methods=["DELETE"])
+router.add_api_route(
+    "/", UserController.index, methods=["GET"], response_model=list[UserPublic]
+)
+router.add_api_route(
+    "/{user_id}", UserController.show, methods=["GET"], response_model=UserPublic
+)
+router.add_api_route(
+    "/", UserController.create, methods=["POST"], response_model=UserPublic
+)
+router.add_api_route(
+    "/{user_id}", UserController.update, methods=["PUT"], response_model=UserPublic
+)
+router.add_api_route(
+    "/{user_id}", UserController.delete, methods=["DELETE"], response_model=dict
+)
