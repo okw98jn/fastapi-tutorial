399f4b1be740f78d3014c63e4964a8cce2746b5b
okw98jn <j.ookawa.0417@gmail.com>
2024-05-16 23:01:28 +0900

    例外処理記述

--- src/settings/db.py
+++ src/settings/db.py
@@ -1,14 +1,28 @@
 import os
+from urllib.parse import quote_plus
 
 from dotenv import load_dotenv
 
+from src.logger import logging
+
 load_dotenv()
 
-DB_USERNAME = os.environ.get("DB_USERNAME")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_HOST = os.environ.get("DB_HOST")
-DB_DATABASE = os.environ.get("DB_DATABASE")
 
-DATABASE_URL = "mysql+pymysql://{}:{}@{}/{}".format(
-    DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DATABASE
+def get_env_variable(name: str) -> str:
+    value = os.environ.get(name)
+    if value is None:
+        error_message = f"Environment variable {name} not set."
+        logging.error(error_message)
+        raise EnvironmentError(error_message)
+    return value
+
+
+DB_USERNAME = get_env_variable("DB_USERNAME")
+DB_PASSWORD = get_env_variable("DB_PASSWORD")
+DB_HOST = get_env_variable("DB_HOST")
+DB_DATABASE = get_env_variable("DB_DATABASE")
+
+DATABASE_URL = (
+    f"mysql+pymysql://{quote_plus(DB_USERNAME)}:{quote_plus(DB_PASSWORD)}"
+    f"@{DB_HOST}/{DB_DATABASE}"
 )
