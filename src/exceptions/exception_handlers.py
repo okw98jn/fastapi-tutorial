e06bca7600f81b590914209671b9488160e2be28
okw98jn <j.ookawa.0417@gmail.com>
2024-05-17 23:51:36 +0900

    500エラーハンドラー作成

--- src/exceptions/exception_handlers.py
+++ src/exceptions/exception_handlers.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from src.exceptions.generic_exception import generic_exception_handler
 from src.exceptions.not_found_exception import (
     NotFoundException,
     not_found_exception_handler,
@@ -8,3 +9,4 @@ from src.exceptions.not_found_exception import (
 
 def add_exception_handlers(app: FastAPI):
     app.add_exception_handler(NotFoundException, not_found_exception_handler)
+    app.add_exception_handler(Exception, generic_exception_handler)
