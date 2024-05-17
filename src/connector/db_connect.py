95dcdb94e67a0509032ee1c5e292d032aaef931d
okw98jn <j.ookawa.0417@gmail.com>
2024-05-17 23:24:19 +0900

    errorログが2重で出力されていたため修正

--- src/connector/db_connect.py
+++ src/connector/db_connect.py
@@ -1,17 +1,16 @@
 from sqlmodel import Session, create_engine
 
-from src.logger import logger
 from src.settings.db import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 
 
 def get_session():
     session = Session(engine)
     try:
         yield session
-    except Exception as e:
-        logger.error(e)
+    except Exception:
+        # ログは例外が箇所で出力する
         session.rollback()
         raise
     finally:
