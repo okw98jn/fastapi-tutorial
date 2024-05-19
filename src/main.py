165a8005ac7ac012696ca43c40609ec0bd88485d
okw98jn <j.ookawa.0417@gmail.com>
2024-05-19 23:53:16 +0900

    ミドルウェアの設定方法変更

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-from starlette.middleware.base import BaseHTTPMiddleware
 
 from src.exceptions.exception_handlers import add_exception_handlers
-from src.middlewares.log import log_middleware
+from src.middlewares.log import LogMiddleware
 from src.routes import auth, user
 
 app = FastAPI()
 
-app.add_middleware(BaseHTTPMiddleware, dispatch=log_middleware)
+app.add_middleware(LogMiddleware)
 
 # エラーハンドラーを一括で登録する
 add_exception_handlers(app)
--- src/middlewares/log.py
+++ src/middlewares/log.py
@@ -1,65 +1,67 @@
 import time
 
 from fastapi import Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from src.settings.logger import logger
 
 
-async def log_middleware(request: Request, call_next) -> Response:
-    """
-    ログミドルウェア
-    """
+class LogMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next) -> Response:
+        """
+        ログミドルウェア
+        """
 
-    start_time = time.time()
-    client_ip = request.client.host if request.client else "Unknown"
+        start_time = time.time()
+        client_ip = request.client.host if request.client else "Unknown"
 
-    # リクエストのログを記録
-    request_body = await request.body()
-    request_log_dict = {
-        "client_ip": client_ip,
-        "url": str(request.url),
-        "method": request.method,
-        "query_params": str(request.query_params),
-        "headers": dict(request.headers),
-        "body": request_body.decode("utf-8"),
-    }
-    logger.info(f"Request: {request_log_dict}")
-
-    try:
-        # レスポンスのログを記録
-        response = await call_next(request)
-        response_body = b""
-        async for chunk in response.body_iterator:
-            response_body += chunk
-
-        response_log_dict = {
+        # リクエストのログを記録
+        request_body = await request.body()
+        request_log_dict = {
             "client_ip": client_ip,
             "url": str(request.url),
             "method": request.method,
-            "status_code": response.status_code,
-            "body": response_body.decode("utf-8"),
-            "processing_time": f"{time.time() - start_time:.4f} seconds",
+            "query_params": str(request.query_params),
+            "headers": dict(request.headers),
+            "body": request_body.decode("utf-8"),
         }
-        logger.info(f"Response: {response_log_dict}")
+        logger.info(f"Request: {request_log_dict}")
 
-        return Response(
-            content=response_body,
-            status_code=response.status_code,
-            headers=dict(response.headers),
-            media_type=response.media_type,
-        )
+        try:
+            # レスポンスのログを記録
+            response = await call_next(request)
+            response_body = b""
+            async for chunk in response.body_iterator:
+                response_body += chunk
 
-    except Exception as e:
-        # 例外発生時のレスポンスログを記録
-        error_response_log_dict = {
-            "client_ip": client_ip,
-            "url": str(request.url),
-            "method": request.method,
-            "status_code": 500,
-            "body": {"detail": "Internal Server Error"},
-            "processing_time": f"{time.time() - start_time:.4f} seconds",
-        }
-        logger.info(f"Response: {error_response_log_dict}")
+            response_log_dict = {
+                "client_ip": client_ip,
+                "url": str(request.url),
+                "method": request.method,
+                "status_code": response.status_code,
+                "body": response_body.decode("utf-8"),
+                "processing_time": f"{time.time() - start_time:.4f} seconds",
+            }
+            logger.info(f"Response: {response_log_dict}")
 
-        # 例外発生時のレスポンスはエラーハンドラーに任せる
-        raise e
+            return Response(
+                content=response_body,
+                status_code=response.status_code,
+                headers=dict(response.headers),
+                media_type=response.media_type,
+            )
+
+        except Exception as e:
+            # 例外発生時のレスポンスログを記録
+            error_response_log_dict = {
+                "client_ip": client_ip,
+                "url": str(request.url),
+                "method": request.method,
+                "status_code": 500,
+                "body": {"detail": "Internal Server Error"},
+                "processing_time": f"{time.time() - start_time:.4f} seconds",
+            }
+            logger.info(f"Response: {error_response_log_dict}")
+
+            # 例外発生時のレスポンスはエラーハンドラーに任せる
+            raise e
